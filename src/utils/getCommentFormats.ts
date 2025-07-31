12acda98e48c5e9487e72202b6b17659e08bedc1
Juraj Stefanic <jurajstefanic@outlook.com>
2025-08-01 01:56:51 +0200

    refactor: simplify comment format handling & add default excluded folders

--- src/utils/getCommentFormats.ts
+++ src/utils/getCommentFormats.ts
@@ -1,82 +1,43 @@
-export function getCommentFormats(ext: string): RegExp[] {
-  switch (ext) {
-    case "js":
-    case "ts":
-    case "jsx":
-    case "tsx":
-      return [
-        /^\s*\/\/.*$/,
-        /^\s*\/\*.*\*\/\s*$/,
-        /^\s*\/\*.*$/,
-        /^\s*\*\/\s*$/,
-        /^\s*\*[^/].*$/,
-      ];
-    case "py":
-    case "ipynb":
-      return [/^\s*#.*/, /^\s*""".*/, /^\s*'''.*/, /^\s*"""/];
-    case "rb":
-      return [/^\s*#.*/, /^\s*=begin.*/, /^\s*=end/];
-    case "lua":
-      return [/^\s*--.*/, /^\s*--\[\[.*/, /^\s*\]\]/];
-    case "html":
-    case "xml":
-    case "svg":
-      return [/^\s*<!--.*/, /^\s*-->/];
-    case "md":
-    case "mdx":
-    case "markdown":
-      return [
-        /^\s*<!--.*/,
-        /^\s*-->/,
-        /^\s*\[comment\]:\s*#.*/,
-        /^\s*\[\/\/\]:\s*#.*/,
-      ];
-    case "sql":
-      return [/^\s*--.*/, /^\s*#.*/, /^\s*\/\*.*/];
-    case "css":
-    case "scss":
-    case "less":
-      return [/^\s*\/\*.*/, /^\s*\*\//, /^\s*\*.*/];
-    case "hs":
-      return [/^\s*--.*/, /^\s*{-.*/, /^\s*-}/];
-    case "lisp":
-    case "clj":
-      return [/^\s*;.*/];
-    case "vim":
-      return [/^\s*".*/];
-    case "c":
-    case "cpp":
-    case "h":
-    case "hpp":
-    case "cs":
-    case "java":
-    case "swift":
-    case "kt":
-    case "scala":
-      return [
-        /^\s*\/\/.*$/,
-        /^\s*\/\*.*\*\/\s*$/,
-        /^\s*\/\*.*$/,
-        /^\s*\*\/\s*$/,
-        /^\s*\*[^/].*$/,
-      ];
-    case "php":
-      return [
-        /^\s*\/\/.*$/,
-        /^\s*#.*$/,
-        /^\s*\/\*.*\*\/\s*$/,
-        /^\s*\/\*.*$/,
-        /^\s*\*\/\s*$/,
-        /^\s*\*[^/].*$/,
-      ];
-    default:
-      return [
-        /^\s*\/\/.*$/,
-        /^\s*\/\*.*\*\/\s*$/,
-        /^\s*\/\*.*$/,
-        /^\s*\*\/\s*$/,
-        /^\s*\*[^/].*$/,
-        /^\s*#.*$/,
-      ];
+export function getCommentFormats(): {
+  singleLine: { [key: string]: RegExp };
+  multiLine: { [key: string]: RegExp };
+} {
+  return {
+    singleLine: {
+      "//": /\/\/(.+)$/,
+      "#": /#(.+)$/,
+      "--": /--(.+)$/,
+      ";": /;(.+)$/,
+    },
+    multiLine: {
+      cStyle: /\/\*[\s\S]*?\*\//g,
+      python: /(?:'''[\s\S]*?''')|(?:"""[\s\S]*?""")/g,
+      html: /<!--[\s\S]*?-->/g,
+    },
+  };
+}
+
+export const DEFAULT_EXCLUDED_FOLDERS = [
+  "node_modules",
+  ".git",
+  ".vscode",
+  "out",
+  "dist",
+  "build",
+  ".next",
+  ".nuxt",
+  "coverage",
+  ".nyc_output",
+  "logs",
+  "tmp",
+  "temp",
+  ".cache",
+];
+
+export function generateExcludePattern(excludedFolders: string[]): string {
+  if (!excludedFolders || excludedFolders.length === 0) {
+    return `**/{${DEFAULT_EXCLUDED_FOLDERS.join(",")}}/**`;
   }
+
+  return `**/{${excludedFolders.join(",")}}/**`;
 }
